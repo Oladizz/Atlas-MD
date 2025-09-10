4645e58876c7acd8b8b83c33f16997653c2b692b
Oldizz <rabiuoladizz@gmail.com>
2025-09-10 00:04:32 +0000

    feat: Update configurations and dependencies
    - Update owner phone number and MongoDB URL in Configurations.js
    
    - Remove duplicate line in Configurations.js
    
    - Update package-lock.json to resolve dependency conflicts

--- Configurations.js
+++ Configurations.js
@@ -2,12 +2,12 @@ require("dotenv").config();
 
 let gg = process.env.MODS;
 if (!gg) {
-  gg = "918101187835";   // You can replace this number with yours //
+  gg = "+2347088230306";   // You can replace this number with yours //
 }
 
 
 global.owner = gg.split(",");
-global.mongodb = process.env.MONGODB || "Put your alternative mongodb url here";
+global.mongodb = process.env.MONGODB || "mongodb+srv://Atlasbot:Oladizz@cluster0.izei6gx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 global.sessionId = process.env.SESSION_ID || "ok";
 global.prefa = process.env.PREFIX || "-";
 global.tenorApiKey = process.env.TENOR_API_KEY || "AIzaSyCyouca1_KKy4W_MG1xsPzuku5oa8W358c";
@@ -15,7 +15,6 @@ global.packname = process.env.PACKNAME || `Atlas Bot`;
 global.author = process.env.AUTHOR || "by: Team Atlas";
 global.port = process.env.PORT || "10000";
 global.openAiAPI = process.env.OPENAI_API || "Put your openai API key here";
-global.owner = gg.split(",");
 
 module.exports = {
   mongodb: global.mongodb,
